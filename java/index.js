3e9227da54ee0b75f4d7f9983368930de983fe6f
Ezequiel <ezequielescobar980@gmail.com>
2022-12-18 21:27:10 -0600

    realizamos lo relacionado con javascript

---
+++ java/index.js
@@ -0,0 +1,75 @@
+
+//variables
+
+//globales
+
+//var saludo = "Bienvenido";
+//if(saludo){
+  //var saludo = "HOLA CUSTOMER";
+  //console.log(saludo);
+//}
+
+//limitadas
+let nombre = "Ezequiel";
+if(nombre){
+  let nombre = "Isabella";
+  console.log(nombre);
+}
+console.log(nombre);
+
+
+
+
+
+//let nombreUsuario = prompt("hola! ¿Cómo te llamas?");
+//const edadUsuario = prompt("Por seguridad de usuario, ¿Qué edad tienes?");
+
+//const edadUser1 = 22
+//const edadUser2 = 30
+//const resultado = edadUser1 + edadUser2;
+
+ //alert ("edad de los dos juntos" + resultado)
+
+
+
+//const edadUser1 = parseInt(prompt("cuantos años tienes"));
+//const edadUser2 = parseInt(prompt("cuantos años tiene el user2"));
+
+//alert(edadUser1 + edadUser2)
+
+
+
+//concatenación
+
+//const datosUsuario = alert ("Hola"  + nombreUsuario  +  ", tienes"  +  edadUsuario +  "años de edad." );
+
+// interpolación
+
+//const saludoTraspolacion  = alert(`HOLA ${nombreUsuario}, tienes ${edadUsuario} años de edad!`)
+
+
+
+//condicional//
+//const edadUsuario = parseInt(prompt("¿cuantos años tienes?"));
+//if (edadUsuario >= 18) {
+   // alert ("pueder entrar y consumir alcohol de forma moderada");
+//} else {
+    //alert ("acceso no permitido,aun por tu edad no puedes entrar, gracias");
+//}
+
+
+
+// mostrarme en consola y en pantalla los 20 nimbre de las personas que asistieron al evento, cn el número de boleto
+
+//let personas  = 1; 
+//for(personas; personas <=20; personas = personas + 1){
+  //  let nombrePersona = prompt("cómo te llamas?");
+ //   console.log ("la persona asitente se llama: ," + nombrePersona + "tendra el boleto número" + personas);
+  //  alert (`La persona se llama: ${nombrePersona} y va a tener el número de boleto de asistencia al evento ${personas}`);
+//}
+
+
+
+
+
+
